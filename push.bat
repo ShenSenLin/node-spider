e004aa8fae8a74ab8dbd3a4b602f3b43cac02298
ShenSenLin <867345686@qq.com>
2025-08-06 15:35:00 +0800

    娣诲姞浼犲弬

--- push.bat
+++ push.bat
@@ -2,7 +2,7 @@ git config --global http.proxy "socks5://127.0.0.1:10808"
 git config --global https.proxy "socks5://127.0.0.1:10808"
 
 git add --all
-git commit -m "none"
+git commit -m "添加传参"
 git push -u origin main
 
 git config --global --unset http.proxy
